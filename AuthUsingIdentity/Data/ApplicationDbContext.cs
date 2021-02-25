092dddf87deaf3cfe6dd12d6d2250dca33ad52ec
mohamedabdelftah15 <51211389+mohamedabdelftah15@users.noreply.github.com>
2021-02-25 15:26:52 +0200

    Add project files.

---
+++ AuthUsingIdentity/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AuthUsingIdentity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthUsingIdentity.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
